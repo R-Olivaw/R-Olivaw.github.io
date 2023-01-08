a812b1ec205e8392fd5fb6386e84881a705fff99
Daneel Olivaw <34151388+R-Olivaw@users.noreply.github.com>
2023-01-07 20:28:19 -0800

    conditions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log("Hello.")
+const userNum = prompt()
+
+if (userNum < 2) {
+    console.log("LOW")
+} else if (userNum <=5) {
+    console.log("KINDA LOW")
+} else if (userNum == 5 ) {
+    console.log("EH")
+} else {
+    console.log("COOL")
+}
