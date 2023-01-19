d3c33402c7ca631edae6ca069480ccb7730bc5d3
Daneel Olivaw <34151388+R-Olivaw@users.noreply.github.com>
2023-01-18 17:47:45 -0800

    guessing game

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
-const userNum = prompt()
+let maximum = parseInt(prompt("Enter your maximum number."));
+while (!maximum) {
+    maximum = parseInt(prompt("Enter a valid number you coniving son of a bitch!"));
+}
 
-if (userNum < 2) {
-    console.log("LOW")
-} else if (userNum <=5) {
-    console.log("KINDA LOW")
-} else if (userNum == 5 ) {
-    console.log("EH")
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+
+let guess = parseInt(prompt("Enter your first guess."));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q') break;
+    attempts++
+    if (guess > targetNum) {
+        guess = prompt("Too high! Haha!");
+    } else {
+        guess = prompt("Too low! Boo Hoo!")
+    }
+}
+if (guess === 'q') {
+    console.log("QUITTER! BOO!")
 } else {
-    console.log("COOL")
+    console.log("WOW CONGRATS BIG BOY!")
+    console.log(`You got it! It took you ${attempts} guesses! WOW!`)
 }
